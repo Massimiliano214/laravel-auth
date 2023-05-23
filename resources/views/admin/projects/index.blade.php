b2d49732962b3f561943b2be903242bc2e3a0eb9
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-05-23 14:50:32 +0200

    added bootrsap delete confirm options

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -20,11 +20,11 @@
                 <td class="d-flex">
                   <a class="btn btn-primary me-3" href="{{route('admin.projects.show', ['project' => $project->slug])}}">Dettagli</a>
                   <a class="btn btn-secondary me-3" href="{{route('admin.projects.edit', ['project' => $project->slug])}}">Modifica</a>
-                  <form action="{{route('admin.projects.destroy', ['project' => $project->slug])}}" method="POST">
+                  <form class="form_delete_post" action="{{route('admin.projects.destroy', ['project' => $project->slug])}}" method="POST">
                       @csrf
                       @method('DELETE')
                       <!--onclick="return confirm('Are you sure?')"-->
-                      <button onclick="return confirm('Are you sure?')" type="submit" class="btn btn-danger delete">Elimina</button>
+                      <button type="submit" class="btn btn-danger delete">Elimina</button>
                   </form>
               </td>
             </tr>
@@ -33,4 +33,21 @@
   </table>
   <a href="{{route('admin.projects.create')}}" class="btn btn-secondary">Aggiungi Progetto</a>
 
+  <div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Conferma eliminazione</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            Confermi di voler eliminare l'elemento selezionato?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-danger">Conferma eliminazione</button>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
